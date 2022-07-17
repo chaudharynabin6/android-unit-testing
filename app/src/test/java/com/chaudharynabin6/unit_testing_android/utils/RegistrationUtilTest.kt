04a070511649c3431c2db191276c2265778b26a1
chaudharynabin6 <chaudharynabin6@gmail.com>
2022-07-17 21:47:53 +0545

    unit testing :   validate Registration

---
+++ app/src/test/java/com/chaudharynabin6/unit_testing_android/utils/RegistrationUtilTest.kt
@@ -0,0 +1,70 @@
+package com.chaudharynabin6.unit_testing_android.utils
+
+import org.junit.Test
+import com.google.common.truth.Truth.assertThat
+
+internal class RegistrationUtilTest {
+
+
+    @Test
+    fun `empty username return false`(){
+
+        val result = RegistrationUtil.validateRegistrationInput(
+            username = "",
+            password = "123",
+            confirmedPassword = "123"
+        )
+        assertThat(result).isFalse()
+    }
+
+
+    @Test
+    fun `valid username and correctly repeated password returns true`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philipp",
+            "123",
+            "123"
+        )
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `username already exists returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Carl",
+            "123",
+            "123"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `incorrectly confirmed password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philipp",
+            "123456",
+            "abcdefg"
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `empty password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philipp",
+            "",
+            ""
+        )
+        assertThat(result).isFalse()
+    }
+
+    @Test
+    fun `less than 2 digit password returns false`() {
+        val result = RegistrationUtil.validateRegistrationInput(
+            "Philipp",
+            "abcdefg5",
+            "abcdefg5"
+        )
+        assertThat(result).isFalse()
+    }
+}
